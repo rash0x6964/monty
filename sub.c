3b3619a3d1023f69ce9516943651766cde3ae104
wikidips <wikidips18@gmail.com>
2023-10-18 21:05:58 +0100

    subtracte

---
+++ sub.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * sub - Subtract the top element of the stack from the second top element
+ * @head: Double pointer to the stack
+ * @line: Line number of the instruction
+ */
+void sub(stack_t **head, unsigned int line)
+{
+	stack_t *temp;
+	int result;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	result = (*head)->next->n - (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	free(temp);
+
+	(*head)->n = result;
+}
+
