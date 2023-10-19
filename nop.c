cf5d83a0be81e8b4a7f30f2b69e08643b90fc74e
wikidips <wikidips18@gmail.com>
2023-10-19 11:13:38 +0100

    no opperation

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: double pointer to the stack
+ * @line_number: unused
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
